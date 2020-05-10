2cd850bea599e416bfa2c3a33d9579bb69c74d28
L-TANK <214927409@qq.com>
2020-05-10 17:55:31 +0800

    实现了变更标签改变下方数据

--- pages/lbsIndex/lbsIndex.js
+++ pages/lbsIndex/lbsIndex.js
@@ -6,9 +6,9 @@ Page({
    */
   data: {
     activeCategory:'',//当前激活的分类
-    categoryData: ['景点', '食堂', '宿舍楼', '图书馆'],
-    coreLongitude: '112.447560',//中心经度
-    coreLatitude: '38.015500',//中心纬度
+    categoryData: ['校门', '学院', '教学楼', '体育设施','食堂','服务建筑','景点','快递站','停车场','校园超市'],
+    coreLongitude: '108.2884222300',//中心经度
+    coreLatitude: '22.8348447400',//中心纬度
     scale: 16,//缩放级别
     markers: [],//标记点
     isSpread: true, //底部可滚动视图区域是否显示，true表示展开，false表示收缩
@@ -21,9 +21,16 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    var that = this
     this.setData({
-      activeCategory: '景点'
+      activeCategory: '校门'
+    })
+    
+    this.getCategory('校门',function(res){
+      var objects = res.data.objects
+      that.setData({
+        merchantsData: objects
+      })
     })
 
     let merchantsData = [
@@ -54,15 +61,37 @@ Page({
       }
     ]
 
-    this.setData({
-      merchantsData: merchantsData
-    })
-
   },
 
   switchMerchantsItems(e) {
     this.setData({
       isSpread: !this.data.isSpread
+    })
+  },
+
+  //获取某一分类下所有标记的数据
+  getCategory:function(name,cb){
+     //实例化tableId,TableObject,query
+     let tableId = 99162
+     let Map = new wx.BaaS.TableObject(tableId)
+     let query = new wx.BaaS.Query()
+     query.in('category', [name])
+     Map.setQuery(query).find().then(res => cb(res), err => {
+       // err
+     })
+  },
+
+  categoryChange:function(e){
+    var that = this
+    var category = e.currentTarget.dataset.category
+    this.setData({
+      activeCategory: category
+    })
+    this.getCategory(category,function(res){
+      var objects = res.data.objects
+      that.setData({
+        merchantsData: objects
+      })
     })
   },
 
