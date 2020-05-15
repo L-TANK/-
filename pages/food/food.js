4ad35e6aff034446b7413ef24e0393343f49e36e
L-TANK <214927409@qq.com>
2020-05-15 13:58:16 +0800

    美食
    添加美食页面

--- pages/food/food.js
+++ pages/food/food.js
@@ -5,14 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    roomName:'',
+    imgUrls:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let tableID = 99566
+    let recordID = '5ebd4e89c66ab41bb9bbb32f'
 
+    let Room = new wx.BaaS.TableObject(tableID)
+
+    Room.get(recordID).then(res => {
+      // success
+      console.log(res.data)
+      this.setData({
+        roomName:res.data.roomName,
+        imgUrls:res.data.image
+      })
+    }, err => {
+      // err
+    })
   },
 
   /**
