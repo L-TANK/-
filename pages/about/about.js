00e4a044ea5b9dcda59d1d33d163fcae7a196b8d
taoke00796 <625522194@qq.com>
2020-05-23 17:38:08 +0800

    js

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,4 +1,6 @@
 // pages/about/about.js
+var wxParse = require('../../utils/wxParse/wxParse.js')
+var config = require('../../config/config');
 Page({
 
   /**
@@ -9,11 +11,12 @@ Page({
     indicatorDots:true,
     autoplay:true,
     interval:3000,
-    navInfo: [ "简介", "历史", "风光" ,"标识"],
-    current: 0,
-    introvideo:"https://www.gxu.edu.cn/__local/2/73/CF/A0048789F2E45A1D52D5FA6A34A_E74CCD54_20F8B80B.mp4?e=.mp4",
-  },
+    schoolName:'',
+    englishName:'',
+    about:'',
 
+  },
+ 
   goToMap: function () {
     wx.navigateTo({
       url: '/pages/lbsIndex/lbsIndex'
@@ -23,10 +26,60 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
+
   onLoad: function (options) {
-  
+   
+    var that = this
+    wx.getStorage({
+      key: 'schoolData',
+      success: function (res) {
+         console.log(res.data)
+        //更新校园简介数据
+        that.setData({
+          schoolName: res.data.schoolName,
+          englishName: res.data.englishName,
+          imgUrls: res.data.image,
+          about:res.data.about
+        })
+        console.log('使用了本地缓存');
+      },
+      fail:function(){
+        console.log('没有使用本地缓存');
+        // 操作school表
+        let tableID =99161
+        let recordID ='5eb76dbadc78475753fc2144'
+
+        let School = new wx.BaaS.TableObject(tableID)
+
+        School.get(recordID).then(res => {
+          // success
+          console.log(res.data)
+          //更新校园简介数据
+          var content = res.data.about
+      WxParse.wxParse('about','html',content,this);
+          that.setData({
+            schoolName: res.data.schoolName,
+            englishName: res.data.englishName,
+            imgUrls: res.data.image,
+            about:res.data.about
+          })
+          //将校园简介数据进行本地缓存
+          wx.setStorage({
+            key: "schoolData",
+            data: res.data
+          })
+        }, err => {
+          // err
+        })
+      }
+    })
+    
+
+
+   
   },
 
+    
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -74,11 +127,5 @@ Page({
    */
   onShareAppMessage: function () {
   
-  },
-
-  updatenavs: function(t) {
-    this.setData({
-        current: t.target.dataset.index,
-    });
-},
+  }
 })
