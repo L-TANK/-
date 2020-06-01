22e7e703ac5311d72f5549599e15a2a5b2198497
L-TANK <214927409@qq.com>
2020-06-01 17:39:45 +0800

    删除介绍页面视频控件 加入滚动框控件

--- pages/about/about.js
+++ pages/about/about.js
@@ -12,13 +12,11 @@ Page({
     interval:3000,
     navInfo: [ "简介", "历史", "风光" ,"标识"], 
     current: 0, 
-    introvideo:"https://www.gxu.edu.cn/__local/2/73/CF/A0048789F2E45A1D52D5FA6A34A_E74CCD54_20F8B80B.mp4?e=.mp4", 
     schoolName:'',
     englishName:'',
     about:'',
     logo:'',
     audio:'',
-    video_img:'',
     xun:'',
     hui:'',
     song:'',
@@ -33,6 +31,7 @@ Page({
       "https://cloud-minapp-35144.cloud.ifanrusercontent.com/1jd6L6d8YH6jrMBG.jpg",
       "https://cloud-minapp-35144.cloud.ifanrusercontent.com/1jd6L6xmHSXnW00F.jpg"
     ],
+    imgUrls: [],
   },
  
   preview(event) {
@@ -67,11 +66,11 @@ Page({
           about:res.data.about, 
           logo:res.data.logo, 
           audio:res.data.audio, 
-          video_img:res.data.video_img,
           xun:res.data.xun,
           hui:res.data.hui,
           song:res.data.song,
-          ge:res.data.ge
+          ge:res.data.ge,
+          imgUrls:res.data.image,
         }) 
       WxParse.wxParse('about','html',res.data.about,this)  
       WxParse.wxParse('xun','html',res.data.xun,this) 
@@ -93,11 +92,11 @@ Page({
             about:res.data.about, 
             logo:res.data.logo, 
             audio:res.data.audio, 
-            video_img:res.data.video_img,
             xun:res.data.xun,
             hui:res.data.hui,
             song:res.data.song,
-            ge:res.data.ge
+            ge:res.data.ge,
+            imgUrls:res.data.image,
           }) 
           WxParse.wxParse('about','html',res.data.about,this) 
           WxParse.wxParse('xun','html',res.data.xun,this) 
